574b139953b455ce3d210e936bbb64bd0a37866a
Jeffrey <ohjeffrey20@gmail.com>
2019-10-02 22:01:31 +1000

    Check TODOs

--- server/functions_tests.py
+++ server/functions_tests.py
@@ -19,8 +19,8 @@ def auth_logout_test():
 def auth_register_test():
     assert auth_register('jeffrey.oh@student.unsw.edu.au', 'right password', 'Jeffrey', 'Oh') == ('12345', 'correct token')
     with pytest.raises(Exception): # Following should raise exceptions
-        assert auth_register('jeffrey.oh@student.unsw.edu.au', 'good password', 'Jeffrey', 'This is a string that is much longer than the max length which is fifty characters long')
-        assert auth_register('jeffrey.oh@student.unsw.edu.au', 'good password', 'This is a string that is much longer than the max length which is fifty characters long', 'Oh')
+        assert auth_register('jeffrey.oh@student.unsw.edu.au', 'good password', 'Jeffrey', 'This is a string that is much longer than the max length')
+        assert auth_register('jeffrey.oh@student.unsw.edu.au', 'good password', 'This is a string that is much longer than the max length', 'Oh')
         assert auth_register('jeffrey.oh@student.unsw.edu.au', 'password that does not meet requirements', 'Jeffrey', 'Oh')
         assert auth_register('bad email', 'good password', 'Jeffrey', 'Oh')
 
@@ -34,3 +34,30 @@ def channel_invite_test():
     # TODO if no output how to test?
 
 def channel_details_test():
+    assert channel_details('valid token', 12345) == ('Jeffrey', ????????) # TODO not sure what the members data type is
+
+def channel_messages_test():
+    # TODO not sure what the messages data type is
+
+def channel_leave_test():
+    # TODO not sure what the messages data type is
+
+def channel_join_test():
+    # TODO not sure what the messages data type is
+
+def channel_addowner_test():
+    # TODO not sure what the messages data type is
+
+def channel_removeowner_test():
+    # TODO not sure what the messages data type is
+
+def channels_list_test ():
+    # TODO yo wtf are these fucking data types
+
+def channels_listall_test():
+    # TODO yo wtf are these fucking data types
+
+def channels_create_test()
+    assert channels_create('valid token', 'Jeffrey', True) == 12345
+    with pytest.raises(Exception): # Following should raise exceptions
+        assert channels_create('valid token', 'This is a string that is much longer than the max length', True)
