79b656874dda9cd2b5a5d665598d5e32b1d64049
Kenny <kennyhan9831@gmail.com>
2019-10-23 22:59:20 +1100

    started writing mesg routes

--- sample_kenny.py
+++ sample_kenny.py
@@ -5,7 +5,6 @@ from json import dumps
 app = Flask(__name__)
 
 # global varaiables
-
 # created a class called Mesg which stores all the information of every message
 class Mesg:
     def __init__(self, sender, create_time, message):
@@ -24,9 +23,20 @@ class Mesg:
     def get_mesg_time():
         return self.create_time
 
+    def set_reaction(self, reaction):
+        self.reaction = reaction
+
+    def remove_reaction(self):
+        self.reaction = None
+
+    def pin_self(self):
+        self.pin = True
+
+    def unpin_self(self):
+        self.pin = False
+
 # a list of messages
 Messages = []
-
 # end of global variable
 
 @app.route('/echo/post', methods = ['POST'])
@@ -40,36 +50,63 @@ def echo_get():
     return dumps({'echo': echo_input})
 
 @app.route('/message/sendlater', methods=['POST'])
+def send_later():
+    global Messages
+    # check TOKEN???
+    # get user???
+    latency = request.form.get('send later')
+    mesg = request.form.get('message')
 
 
 @app.route('/message/send', methods=['POST'])
-def send():
+def mesg_send():
     global Messages
     # check TOKEN???
-    mesg = request.form.get('message')
     # get user???
+    mesg = request.form.get('message')
     sending_time = datetime.now()
-    Messages.append(Mesg(sender, sending_time, mesg))
-    return dumps('message sent')
-
+    if len(mesg) < 1000 or len(mesg) == 1000:
+        Messages.append(Mesg(sender, sending_time, mesg))
+        return dumps('message sent')
+    else:
+        return dumps('message not send due to some error')
 
 @app.route('/message/remove', methods=['DELET'])
-
+def mesg_remove():
+    global Messages
+    # how to find the correct original message?
 
 @app.route('/message/edit', methods=['PUT'])
-
+def mesg_edit():
+    global Messages
+    new_message = request.form.get('new message')
+    # how to find the correct original message?
+    return dumps('successfully edited the message')
 
 @app.route('/message/react', methods=['POST'])
+def mesg_react():
+    global Messages
+    new_reaction = request.form.get('reaction')
+    # how to find the correct original message?
 
 
 @app.route('/message/unreact', methods=['POST'])
+def mesg_unreact():
+    global Messages
+    # how to find the correct original message?
+
 
 
 @app.route('/message/pin', methods=['POST'])
+def mesg_pin():
+    global Messages
+    # how to find the correct original message?
 
 
 @app.route('/message/unpin', methods=['POST'])
-
+def mesg_unpin():
+    global Messages
+    # how to find the correct original message?
 
 
 if __name__ == '__main__':
