ccbb1cb71e2194630227c62edde4e2b724a50afb
Kenny <kennyhan9831@gmail.com>
2019-10-24 09:15:38 +1100

    added mesgs

--- class_defines.py
+++ class_defines.py
@@ -15,3 +15,37 @@ class channel():
         self.admins = []
         self.members = []
         # later: self.messages = [], self.pinned_messages = []
+
+
+# a list of messages
+Messages = []
+
+class Mesg:
+    def __init__(self, sender, create_time, message):
+        self.message = message
+        self.sender = sender
+        self.create_time = create_time
+        self.reaction = None
+        self.pin = False
+
+    def get_mesg():
+        return self.message
+
+    def get_sender():
+        return self.sender
+
+    def get_mesg_time():
+        return self.create_time
+
+    def set_reaction(self, reaction):
+        self.reaction = reaction
+
+    def remove_reaction(self):
+        self.reaction = None
+
+    def pin_self(self):
+        self.pin = True
+
+    def unpin_self(self):
+        self.pin = False
+
