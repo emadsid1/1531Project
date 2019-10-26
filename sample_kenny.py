416cc234c0fe66a31dbfb6897bfd8e203181deaa
Kenny <kennyhan9831@gmail.com>
2019-10-26 23:23:50 +1100

    finished remove, send and send later

--- sample_kenny.py
+++ sample_kenny.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 # helper functions
 
 # find the correct channel base on the channel_id
-def find_channel(channel_id):
+def find_channel(chan_id):
     global data
     channel_found = False
     for cha in data['channels']:
-        if cha.channel_id == channel_id:
+        if cha.channel_id == chan_id:
             channel_found = True
             return cha
     if channel_found == False:
@@ -32,6 +32,17 @@ def find_msg(channel, msg_id):
     if message_found == False:
         raise AccessError('Message does not exist in current channel!')
 
+# check if a user is an admin or an owner of a given channel
+def check_owner_admin(channel, user_id):
+    global data
+    for u_id in channel.admins:
+        if u_id = user_id:
+            return True
+    for u_id in channel.owners:
+        if u_id = user_id:
+            return True
+    return False
+
 # end of helper functions
 
 @app.route('/echo/post', methods = ['POST'])
@@ -47,12 +58,24 @@ def echo_get():
 @app.route('/message/sendlater', methods=['POST'])
 def send_later():
     global data
-    valid_send = False  # check if the user can actually send the message
-    valid_account = False   # check if the sender is in the account list
-    sender = request.form.get('user')
-    time_to_send = request.form.get('time to send')
+    token = request.form.get('token')
     msg = request.form.get('message')
-
+    chan_id = request.form.get('channel id')
+    time_sent = request.form.get('time sent')
+    if len(msg) > 1000:
+        raise ValueError('Message is more than 1000 words!')
+    else:
+        sender = user_from_token(token)
+        current_channel = find_channel(chan_id)
+        msg_id = str(uuid4())
+        while 1:                                    # TODO not sure if this is right
+            if datetime.now() = time_sent:
+                break
+        current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, chan_id, True))
+    return dumps({
+        'message_id': msg_id,    
+    })
+    
 @app.route('/message/send', methods=['POST'])
 def mesg_send():
     global data
@@ -64,18 +87,31 @@ def mesg_send():
         raise ValueError('Message is more than 1000 words!')
     else:
         sender = user_from_token(token)
-        current_channel = find_channel
+        current_channel = find_channel(chan_id)
+        # generate a globally unique id
         msg_id = str(uuid4())
-        current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, False))
+        # no exceptions raised, then add(send) the message to the current channel
+        current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, chan_id, False))
     return dumps({
-        'message_id': msg_id,    # TODO not sure if this is right
+        'message_id': msg_id,
     })
 
-@app.route('/message/remove', methods=['DELET'])
+@app.route('/message/remove', methods=['DELET'])    # TODO no channel id???????
 def mesg_remove():
     global data
-    remover = request.form.get('user')
-    current_channel = request.form.get('channel')
+    token = request.form.get('token')
+    msg_id = request.form.get('message id')
+    remover = user_from_token(token)
+    found_msg = find_msg(msg_id)
+    # find the channel where the message belongs to
+    msg_channel = find_channel(found_msg.in_channel)
+    if found_msg.sender != remover:
+        raise AccessError('You do not have the permission to delete this message as you are not the sender!')
+    elif check_owner_admin(msg_channel, remover.user_id) == False:
+        raise AccessError('You do not have the permission as you are not the owner or admin of this channel!')
+    # no exception raised, then remove the message
+    msg_channel.messages.remove(found_msg)
+    return dumps
 
 @app.route('/message/edit', methods=['PUT'])
 def mesg_edit():
