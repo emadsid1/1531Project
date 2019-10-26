b656196fe8b65f1d39149bf18f589d634c9d20e8
Kenny <kennyhan9831@gmail.com>
2019-10-26 17:49:28 +1100

    changed message/send

--- sample_kenny.py
+++ sample_kenny.py
@@ -1,9 +1,38 @@
-from class_defines import data, user, channel, mesg, reacts
 from flask import Flask, request, flash
 from datetime import datetime
+from uuid import uuid4
 from json import dumps
+from Error import AccessError
+from class_defines import data, user, channel, mesg, reacts
+from sample_emad import user_from_token, user_from_uid
 
 app = Flask(__name__)
+
+# helper functions
+
+# find the correct channel base on the channel_id
+def find_channel(channel_id):
+    global data
+    channel_found = False
+    for cha in data['channels']:
+        if cha.channel_id == channel_id:
+            channel_found = True
+            return cha
+    if channel_found == False:
+        raise AccessError('Channel does not exit, please join or create a channel first!')
+
+# find the correct message base on the message_id
+def find_msg(channel, msg_id):
+    global data
+    message_found = False
+    for msg in channel.messages:
+        if msg.message_id == msg_id:
+            message_found = True
+            return msg
+    if message_found == False:
+        raise AccessError('Message does not exist in current channel!')
+
+# end of helper functions
 
 @app.route('/echo/post', methods = ['POST'])
 def echo_post():
@@ -24,60 +53,36 @@ def send_later():
     time_to_send = request.form.get('time to send')
     msg = request.form.get('message')
 
-
 @app.route('/message/send', methods=['POST'])
 def mesg_send():
     global data
-    valid_send = False  # check if the user can actually send the message
-    valid_account = False   # check if the sender is in the account list
     sending_time = datetime.now()
-    sender = request.form.get('user')
+    token = request.form.get('token')
     msg = request.form.get('message')
-    current_channel = request.form.get('channel')
+    chan_id = request.form.get('channel id')
     if len(msg) > 1000:
-        raise Exception('ValueError')
+        raise ValueError('Message is more than 1000 words!')
     else:
-        for counter, acc in enumerate(data['accounts']):
-            if acc == sender:
-                # sender exists in the account list, get the token and channels the sender holds
-                valid_account = True
-                token = acc.token
-                user_channels = acc.in_channel
-                if token == '':
-                    # if the sender is not authorised
-                    raise Exception('AccessError')
-                else:
-                    for cha in user_channels:
-                        if current_channel == cha:
-                            # sender verified and add the new message to the current channel
-                            valid_send = True
-                    if valid_send == False:
-                        # the authorised user has not joined the channel they are trying to post to
-                        raise Exception('AccessError')
-        if valid_account == False:
-            # if the account doesn't exist in the account list
-            raise Exception('AccessError')
-    if valid_send == True and valid_account == True:
-        for cha in user_channels:
-            if cha == current_channel:
-                cha.messages.append(mesg(sender, sending_time, msg, False))
-                cha.messages.message_id = len(cha.messages)     # TODO not sure if this is right
-                break
+        sender = user_from_token(token)
+        current_channel = find_channel
+        msg_id = str(uuid4())
+        current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, False))
     return dumps({
-        'message_id': cha.messages.message_id,    # TODO not sure if this is right
+        'message_id': msg_id,    # TODO not sure if this is right
     })
 
 @app.route('/message/remove', methods=['DELET'])
 def mesg_remove():
     global data
-    # how to find the correct original message?
+    remover = request.form.get('user')
+    current_channel = request.form.get('channel')
 
 @app.route('/message/edit', methods=['PUT'])
 def mesg_edit():
     global data
     new_message = request.form.get('new message')
     # how to find the correct original message?
-    return dumps('successfully edited the message')
+    
 
 @app.route('/message/react', methods=['POST'])
 def mesg_react():
