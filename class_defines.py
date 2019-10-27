fd008a7b614311e7a8ab79b9374963c789dc9a02
Kenny <kennyhan9831@gmail.com>
2019-10-27 12:28:50 +1100

    changed one line in mesg

--- class_defines.py
+++ class_defines.py
@@ -38,7 +38,7 @@ class mesg:
     def __init__(self, sender, create_time, message, message_id, channel_id, is_later):
         self.message = message          # string
         self.message_id = message_id    # string of a unique id
-        self.sender = sender            # type user
+        self.sender = sender            # type user_id
         self.create_time = create_time  # date_time (depends on is_later)
         self.in_channel = channel_id    # channel id of which the message is blong to
         self.reaction = None            # active reaction
