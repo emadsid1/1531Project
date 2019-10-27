d383dfa05a2db1c2aeb99af4e6e65d16c8e8e381
Kenny <kennyhan9831@gmail.com>
2019-10-27 13:42:17 +1100

    finished and tested all messages except sendlater

--- sample_kenny.py
+++ sample_kenny.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from datetime import datetime
+from datetime import datetime, timezone
 from uuid import uuid4
 from json import dumps
 from Error import AccessError
@@ -24,8 +24,8 @@ def find_channel(chan_id):
 def find_msg(msg_id):
     global data
     message_found = False
-    for chan in data.channels:
-        for msg in channel.messages:
+    for chan in data['channels']:
+        for msg in chan.messages:
             if msg.message_id == msg_id:
                 message_found = True
                 return msg
@@ -33,24 +33,24 @@ def find_msg(msg_id):
         raise ValueError('Message does not exists!')
 
 # check if a user is an owner of a given channel
-def check_owner(channel, user_id):
+def check_owner(channel, u_id):
     global data
-    for u_id in channel.owners:
-        if u_id = user_id:
+    for user_id in channel.owners:
+        if user_id == u_id:
             return True
     return False
 
 # check if a user is an admin of a given channel
-def check_admin(channel, user_id):
-    for u_id in channel.admins:
-        if u_id = user_id:
+def check_admin(channel, u_id):
+    for user_id in channel.admins:
+        if user_id == u_id:
             return True
     return False
-    
+
 # check if a user is an member of a given channel
-def check_member(channel, user_id):
-    for u_id in channel.members:
-        if u_id = user_id:
+def check_member(channel, u_id):
+    for user_id in channel.members:
+        if user_id == u_id:
             return True
     return False
 # end of helper functions
@@ -68,10 +68,24 @@ def echo_get():
 @app.route('/message/sendlater', methods=['POST'])
 def send_later():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
     msg = request.form.get('message')
-    chan_id = int(request.form.get('channel id'))
-    time_sent = request.form.get('time sent')
+    chan_id = int(request.form.get('channel_id'))
+    sent_stamp = int(request.form.get('time_sent'))
+    # get the actual datetime object from a integer stamp input
+    dt_sent = datetime.fromtimestamp(sent_stamp)
     if len(msg) > 1000:
         raise ValueError('Message is more than 1000 words!')
     else:
@@ -79,7 +93,7 @@ def send_later():
         current_channel = find_channel(chan_id)
         msg_id = int(uuid4())
         while 1:                                    # TODO not sure if this is right
-            if datetime.now() = time_sent:
+            if datetime.now() == dt_sent:
                 break
         current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, chan_id, True))
     return dumps({
@@ -89,35 +103,53 @@ def send_later():
 @app.route('/message/send', methods=['POST'])
 def mesg_send():
     global data
+
+    # TESTING
+    # data['accounts'].append(user('email', 'password', 'first', 'last', 'handle', 'token', 12345))
+    # data['channels'].append(channel('kenny channel', True, 123456, 15))
+    # TESTING
+
     sending_time = datetime.now()
     token = request.form.get('token')
     msg = request.form.get('message')
-    chan_id = int(request.form.get('channel id'))
+    chan_id = int(request.form.get('channel_id'))
     if len(msg) > 1000:
         raise ValueError('Message is more than 1000 words!')
     else:
         sender = user_from_token(token)
         current_channel = find_channel(chan_id)
         # generate a globally unique id
-        msg_id = str(uuid4())
+        msg_id = int(uuid4())
         # no exceptions raised, then add(send) the message to the current channel
         current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, chan_id, False))
     return dumps({
         'message_id': msg_id,
     })
 
-@app.route('/message/remove', methods=['DELET'])    # TODO no channel id???????
+@app.route('/message/remove', methods=['DELETE'])    # TODO no channel id???????
 def mesg_remove():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
-    msg_id = int(request.form.get('message id'))
+    msg_id = int(request.form.get('message_id'))
     remover = user_from_token(token)
     found_msg = find_msg(msg_id)
     # find the channel where the message belongs to
     msg_channel = find_channel(found_msg.in_channel)
     if found_msg.sender != remover:
         raise AccessError('You do not have the permission to delete this message as you are not the sender!')
-    elif (check_owner(msg_channel, remover.user_id) == False) or (check_admin(msg_channel, remover.user_id) == False):
+    elif (check_owner(msg_channel, remover.u_id) == False) or (check_admin(msg_channel, remover.u_id) == False):
         raise AccessError('You do not have the permission as you are not the owner or admin of this channel!')
     # no exception raised, then remove the message
     msg_channel.messages.remove(found_msg)
@@ -128,9 +160,21 @@ def mesg_remove():
 @app.route('/message/edit', methods=['PUT'])
 def mesg_edit():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
-    msg_id = int(request.form.get('message id'))
-    new_message = request.form.get('new message')
+    msg_id = int(request.form.get('message_id'))
+    new_message = request.form.get('message')
     editor = user_from_token(token)
     found_msg = find_msg(msg_id)
     msg_channel = find_channel(found_msg.in_channel)
@@ -138,7 +182,7 @@ def mesg_edit():
         raise ValueError('Message is more than 1000 words!')
     elif found_msg.sender != editor:
         raise AccessError('You do not have the permission to edit this message as you are not the sender!')
-    elif (check_owner(msg_channel, editor.user_id) == False) or (check_admin(msg_channel, editor.user_id) == False):
+    elif (check_owner(msg_channel, editor.u_id) == False) or (check_admin(msg_channel, editor.u_id) == False):
         raise AccessError('You do not have the permission as you are not the owner or admin of this channel!')
     # edit the message if no exceptions raiseds
     found_msg.message = new_message
@@ -149,9 +193,21 @@ def mesg_edit():
 @app.route('/message/react', methods=['POST'])
 def mesg_react():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
-    msg_id = request.form.get('message id')
-    react_id = request.form.get('react id')
+    msg_id = int(request.form.get('message_id'))
+    react_id = int(request.form.get('react_id'))
     reacter = user_from_token(token)
     found_msg = find_msg(msg_id)
     if react_id != 1:
@@ -167,29 +223,56 @@ def mesg_react():
 @app.route('/message/unreact', methods=['POST'])
 def mesg_unreact():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
-    msg_id = request.form.get('message id')
-    react_id = request.form.get('react id')
+    msg_id = int(request.form.get('message_id'))
+    react_id = int(request.form.get('react_id'))
     found_msg = find_msg(msg_id)
     if react_id != 1:
         raise ValueError('Invalid React ID!')
     elif found_msg.reaction == None:
         raise ValueError('This message does not contain an active React!')
     found_msg.reaction = None
+    return dumps({
+
+    })
 
 @app.route('/message/pin', methods=['POST'])
 def mesg_pin():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
-    msg_id = int(request.form.get('message id'))
+    msg_id = int(request.form.get('message_id'))
     pinner = user_from_token(token)
     found_msg = find_msg(msg_id)
     msg_channel = find_channel(found_msg.in_channel)
-    if check_admin(msg_channel, pinner.user_id) == False:
+    if check_admin(msg_channel, pinner.u_id) == False:
         raise ValueError('You can not pin the message as you are not an Admin of the channel')
     elif found_msg.is_pinned == True:
         raise ValueError('The message is already pinned!')
-    elif check_member(msg_channel, pinner.user_id) == False:
+    elif check_member(msg_channel, pinner.u_id) == False:
         raise AccessError('You can not pin the message as you are not a member of the channel')
     # pin the message if no exceptions raised
     found_msg.is_pinned = True
@@ -200,16 +283,28 @@ def mesg_pin():
 @app.route('/message/unpin', methods=['POST'])
 def mesg_unpin():
     global data
+
+    # TESTING
+    # user1 = user('email', 'password', 'first', 'last', 'handle', 'token', 1111)
+    # channel1 = channel('kenny channel', True, 123456, 15)
+    # data['accounts'].append(user1)
+    # data['channels'].append(channel1)
+    # data['channels'][0].owners.append(user1.u_id)
+    # data['channels'][0].admins.append(user1.u_id)
+    # data['channels'][0].members.append(user1.u_id)
+    # data['channels'][0].messages.append(mesg(user1, datetime.now(), 'hello world', 54321, 123456, False))
+    # TESTING
+
     token = request.form.get('token')
-    msg_id = int(request.form.get('message id'))
+    msg_id = int(request.form.get('message_id'))
     unpinner = user_from_token(token)
     found_msg = find_msg(msg_id)
     msg_channel = find_channel(found_msg.in_channel)
-    if check_admin(msg_channel, unpinner.user_id) == False:
+    if check_admin(msg_channel, unpinner.u_id) == False:
         raise ValueError('You can not unpin the message as you are not an Admin of the channel')
     elif found_msg.is_pinned == False:
         raise ValueError('The message is already unpinned!')
-    elif check_member(msg_channel, unpinner.user_id) == False:
+    elif check_member(msg_channel, unpinner.u_id) == False:
         raise AccessError('You can not unpin the message as you are not a member of the channel')
     # unpin the message if no exceptions raised
     found_msg.is_pinned = False
