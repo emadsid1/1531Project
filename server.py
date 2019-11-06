b9ee823eba49c98231c36b8c93a3360c9c22820c
Kenny <kennyhan9831@gmail.com>
2019-11-06 15:14:29 +1100

    fixed all message route functions

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ from datetime import datetime, timezone, timedelta
 from Error import AccessError
 from class_defines import data, User, channel, mesg, reacts
 from auth import auth_login, auth_logout, auth_register, reset_request, reset_reset
-from helper_functions import *
+from message_functions import send_later, msg_send, msg_remove, msg_edit, msg_pin, msg_unpin, msg_react, msg_unreact
 
 app = Flask(__name__)
 CORS(app)
@@ -308,173 +308,58 @@ def channel_messages():
     })
 
 @app.route('/message/sendlater', methods=['POST'])
-def send_later():
-    global data
+def route_send_later():
     token = request.form.get('token')
     msg = request.form.get('message')
     chan_id = int(request.form.get('channel_id'))
     sent_stamp = int(request.form.get('time_sent'))
-    # get the actual datetime object from a integer stamp input
-    dt_sent = datetime.fromtimestamp(sent_stamp)
-    if len(msg) > 1000:
-        raise ValueError('Message is more than 1000 words!')
-    elif dt_sent < datetime.now():
-        raise ValueError('Time sent is a value in the past!')
-    else:
-        sender = user_from_token(token)
-        current_channel = find_channel(chan_id)
-        # check if the sender has joined the channel
-        has_joined = False
-        for chan in sender.in_channel:
-            if find_channel(chan) == current_channel:
-                has_joined == True
-        if has_joined == False:
-            raise AccessError('You have not joined this channel yet, join first!')
-        # generate a globally unique id
-        msg_id = int(uuid4())
-        while 1:
-            if datetime.now() == dt_sent:
-                break
-        # time sent reached and no exceptions raised, send the message
-        current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, chan_id, True))
-    return dumps({
-        'message_id': msg_id,
-    })
+    return send_later(token, msg, chan_id, sent_stamp)
 
 @app.route('/message/send', methods=['POST'])
-def mesg_send():
-    global data
-    sending_time = datetime.now()
+def route_msg_send():
     token = request.form.get('token')
     msg = request.form.get('message')
     chan_id = int(request.form.get('channel_id'))
-    if len(msg) > 1000:
-        raise ValueError('Message is more than 1000 words!')
-    else:
-        sender = user_from_token(token)
-        current_channel = find_channel(chan_id)
-        # generate a globally unique id
-        msg_id = int(uuid4())
-        # no exceptions raised, then add(send) the message to the current channel
-        current_channel.messages.append(mesg(sender, sending_time, msg, msg_id, chan_id, False))
-    return dumps({
-        'message_id': msg_id,
-    })
+    return msg_send(token, msg, chan_id)
 
-@app.route('/message/remove', methods=['DELETE'])    # TODO no channel id???????
-def mesg_remove():
-    global data
+@app.route('/message/remove', methods=['DELETE'])
+def route_msg_remove():
     token = request.form.get('token')
     msg_id = int(request.form.get('message_id'))
-    remover = user_from_token(token)
-    found_msg = find_msg(msg_id)
-    # find the channel where the message belongs to
-    msg_channel = find_channel(found_msg.in_channel)
-    if found_msg.sender != remover:
-        raise AccessError('You do not have the permission to delete this message as you are not the sender!')
-    elif (check_owner(msg_channel, remover.u_id) == False) or (check_admin(msg_channel, remover.u_id) == False):
-        raise AccessError('You do not have the permission as you are not the owner or admin of this channel!')
-    # no exception raised, then remove the message
-    msg_channel.messages.remove(found_msg)
-    return dumps({
-
-    })
+    return msg_remove(token, msg_id)
 
 @app.route('/message/edit', methods=['PUT'])
-def mesg_edit():
-    global data
+def route_msg_edit():
     token = request.form.get('token')
     msg_id = int(request.form.get('message_id'))
     new_message = request.form.get('message')
-    editor = user_from_token(token)
-    found_msg = find_msg(msg_id)
-    msg_channel = find_channel(found_msg.in_channel)
-    if len(new_message) > 1000:
-        raise ValueError('Message is more than 1000 words!')
-    elif found_msg.sender != editor:
-        raise AccessError('You do not have the permission to edit this message as you are not the sender!')
-    elif (check_owner(msg_channel, editor.u_id) == False) or (check_admin(msg_channel, editor.u_id) == False):
-        raise AccessError('You do not have the permission as you are not the owner or admin of this channel!')
-    # edit the message if no exceptions raiseds
-    found_msg.message = new_message
-    return dumps({
-
-    })
+    return msg_edit(token, msg_id, new_message)
 
 @app.route('/message/react', methods=['POST'])
-def mesg_react():
-    global data
+def route_msg_react():
     token = request.form.get('token')
     msg_id = int(request.form.get('message_id'))
     react_id = int(request.form.get('react_id'))
-    reacter = user_from_token(token)
-    found_msg = find_msg(msg_id)
-    if react_id != 1:
-        raise ValueError('Invalid React ID!')
-    elif found_msg.reaction != None:
-        raise ValueError('This message already contains an active React!')
-    # give the message a reaction if no exceptions raised
-    found_msg.reaction = reacts(reacter, react_id)
-    return dumps({
-
-    })
+    return msg_react(token, msg_id, react_id)
 
 @app.route('/message/unreact', methods=['POST'])
-def mesg_unreact():
-    global data
+def route_msg_unreact():
     token = request.form.get('token')
     msg_id = int(request.form.get('message_id'))
     react_id = int(request.form.get('react_id'))
-    found_msg = find_msg(msg_id)
-    if react_id != 1:
-        raise ValueError('Invalid React ID!')
-    elif found_msg.reaction == None:
-        raise ValueError('This message does not contain an active React!')
-    # unreact the message if no exceptions raised
-    found_msg.reaction = None
-    return dumps({
-
-    })
+    return msg_unreact(toekn, msg_id, react_id)
 
 @app.route('/message/pin', methods=['POST'])
-def mesg_pin():
-    global data
+def route_msg_pin():
     token = request.form.get('token')
     msg_id = int(request.form.get('message_id'))
-    pinner = user_from_token(token)
-    found_msg = find_msg(msg_id)
-    msg_channel = find_channel(found_msg.in_channel)
-    if check_admin(msg_channel, pinner.u_id) == False:
-        raise ValueError('You can not pin the message as you are not an Admin of the channel')
-    elif found_msg.is_pinned == True:
-        raise ValueError('The message is already pinned!')
-    elif check_member(msg_channel, pinner.u_id) == False:
-        raise AccessError('You can not pin the message as you are not a member of the channel')
-    # pin the message if no exceptions raised
-    found_msg.is_pinned = True
-    return dumps({
-
-    })
+    return msg_pin(token, msg_id)
 
 @app.route('/message/unpin', methods=['POST'])
-def mesg_unpin():
-    global data
+def route_msg_unpin():
     token = request.form.get('token')
     msg_id = int(request.form.get('message_id'))
-    unpinner = user_from_token(token)
-    found_msg = find_msg(msg_id)
-    msg_channel = find_channel(found_msg.in_channel)
-    if check_admin(msg_channel, unpinner.u_id) == False:
-        raise ValueError('You can not unpin the message as you are not an Admin of the channel')
-    elif found_msg.is_pinned == False:
-        raise ValueError('The message is already unpinned!')
-    elif check_member(msg_channel, unpinner.u_id) == False:
-        raise AccessError('You can not unpin the message as you are not a member of the channel')
-    # unpin the message if no exceptions raised
-    found_msg.is_pinned = False
-    return dumps({
-
-    })
+    return msg_unpin(token, msg_id)
 
 @app.route('/user/profile', methods=['GET'])
 def route_user_profile():
