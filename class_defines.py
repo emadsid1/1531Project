60783bcefe0bfabb3e92f056ff2c1967505e52aa
Emad Siddiqui <e.siddiqui@student.unsw.edu.au>
2019-11-10 23:08:17 +1100

    tests

--- class_defines.py
+++ class_defines.py
@@ -55,7 +55,7 @@ class Threads(threading.Thread):
         self.time = time                # either standup_end time or message_send time
 
 data = {
-    'accounts': [User('chiefjief5@gmail.com', '123456', 'Jeffrey', 'Oh', 'JeffreyOh', '1234', 0), User('kennyhan9831@gmail.com', '1234567', 'Jun', 'Han', 'JunHan', '12345', 1)],
+    'accounts': [], #User('chiefjief5@gmail.com', '123456', 'Jeffrey', 'Oh', 'JeffreyOh', '1234', 0), User('kennyhan9831@gmail.com', '1234567', 'Jun', 'Han', 'JunHan', '12345', 1)
     'channels': [Channel('Mychannel', True, 787, False)],
     'account_count': 0
 }
